5741d605df7b47e117715f468fac103b7803c6df
lakshya <lakshya10171@gmail.com>
2025-10-24 11:25:20 +0530

    basic commit

--- src/main/java/com/carGo/notification_consumer_service/NotificationConsumerServiceApplication.java
+++ src/main/java/com/carGo/notification_consumer_service/NotificationConsumerServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NotificationConsumerServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Setup Notification System");
 		SpringApplication.run(NotificationConsumerServiceApplication.class, args);
 	}
 
