f396acbb5b1206d6e33a0d059c94e4ad5a112b95
lakshya <lakshya10171@gmail.com>
2025-10-27 11:03:08 +0530

    add NotificationConsumer Service

--- src/main/java/com/carGo/notification_consumer_service/NotificationConsumerServiceApplication.java
+++ src/main/java/com/carGo/notification_consumer_service/NotificationConsumerServiceApplication.java
@@ -2,7 +2,7 @@ package com.carGo.notification_consumer_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+ 
 @SpringBootApplication
 public class NotificationConsumerServiceApplication {
 
